6c77a553fe5e9e71b07e71976478d1fb85258fe9
Luis Kalckstein <64163735+AltesHaus@users.noreply.github.com>
2025-01-26 23:19:25 +0100

    documentation

--- GDriveBackup.sh
+++ GDriveBackup.sh
@@ -12,13 +12,13 @@ install_gupload() {
 create_backup_zip() {
     backup_file_name="backup_$(date '+%Y-%m-%d_%H-%M-%S').zip"
     echo "Creating backup ZIP file: $backup_file_name"
-    zip -r "$backup_file_name" ./*
+    zip -r "$backup_file_name" ./* #TODO: ensure availability
     echo "$backup_file_name"
 }
 
 upload_to_drive() {
     local file="$1"
-    local backup_folder="BACKUP"
+    local backup_folder="BACKUP" #TODO: accepting custom flags
 
     echo "Checking for existing BACKUP folder in Google Drive..."
     folder_id=$(gupload --list | grep "$backup_folder" | awk '{print $1}')
@@ -29,7 +29,7 @@ upload_to_drive() {
     fi
 
     echo "Uploading $file to Google Drive folder: $backup_folder"
-    gupload --folder "$folder_id" "$file"
+    gupload --folder "$folder_id" "$file" 
 }
 
 # Main script execution
@@ -37,11 +37,11 @@ install_gupload
 
 backup_file=$(create_backup_zip)
 
-if [ ! -f "${HOME}/.googledrive.conf" ]; then
+if [ ! -f "${HOME}/.googledrive.conf" ]; then #Checking for config file
     read -p "Do you already have a Gupload config file? (y/n): " conf
     if [ "$conf" == "y" ]; then
         read -p "Enter your file-path here (Format: /path/to/.googledrive.conf): " path
-        gupload --config "$path" "$backup_file"
+        gupload --config "$path" "$backup_file" #TODO: check for existence first
     else
         echo "Follow this tutorial to configure your credentials:"
         echo "https://labbots.github.io/google-drive-upload/setup/initial_setup/#enable-drive-api"
@@ -53,6 +53,6 @@ fi
 
 # Remove the local ZIP file after uploading
 echo "Removing local ZIP file: $backup_file"
-rm "$backup_file"
+rm "$backup_file" #TODO: Ensure existence
 
 echo "Backup completed successfully."
